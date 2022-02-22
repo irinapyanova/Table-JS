0823fb8f2a4884055eb28da70e93f2c7b5c95a50
Irina Pyanova <kizaru001@gmail.com>
2022-02-22 09:58:29 +0300

    Скрытие столбца таблицы

--- scripts/script.js
+++ scripts/script.js
@@ -5,54 +5,53 @@ const tableTh = document.querySelectorAll('th');
 let rowId;
 
 function sortData(res) {
-
-  const a = res[0].name.firstName;
-
+  let column;
+  let order;
 
   tableTh.forEach(item => {
     item.addEventListener('click', (e) => {
       const target = e.target;
-      const column = target.getAttribute('data-column') == 'asc' ? 'desc' : 'asc';
-      const order = target.getAttribute('data-order');
-      
-        target.setAttribute('data-order', order);
-        res = res.sort((a, b) => {
-          if (a[column] < b[column]) {
-            return order == 'asc' ? 1 : -1;
-          }
-          if (a[column] >= b[column]) {
-            
-            return order == 'asc' ? -1 : 0;
-          }
-          
-          return 0;
-        })
+      column = target.getAttribute('data-column');
+      column = column.split(' ');
+      let len = column.length;
 
-        console.log(res);
+      order = target.getAttribute('data-order')  == 'asc' ? 'desc' : 'asc';
       
+      target.setAttribute('data-order', order);
+
+      res = res.sort((a, b) => {
+        let i = 0;
+
+        while( i < len ) { a = a[column[i]]; b = b[column[i]]; i++; }
+        if (a < b) {
+          return order == 'asc' ? 1 : -1;
+        } else if (a > b) {
+          return order == 'asc' ? -1 : 0;
+        } else {
+          return 0;
+        }
+      })
       renderItems(res);
     })
     
   })
 
-  
 }
 
 
 const renderItems = (data) => {
   tableBody.innerHTML = '';
   
-  data.forEach((item, i) => {
+  data.forEach(item => {
     
     const {about, eyeColor, name} = item;
     const tr = document.createElement('tr');
     tr.classList.add('row');
     tr.innerHTML = ` 
-      <td>${i + 1}</td>
-      <td>${name.firstName}</td>
-      <td>${name.lastName}</td>
-      <td class="about">${about}</td>
-      <td class="color">${eyeColor}</td>
+      <td id="fname-head" >${name.firstName}</td>
+      <td id="lname-head" >${name.lastName}</td>
+      <td id="about-head" class="about">${about}</td>
+      <td id="color-head"  class="color" style="color: ${eyeColor}">${eyeColor}</td>
     `;
 
     tableBody.append(tr);
@@ -64,19 +63,16 @@ const renderItems = (data) => {
       let arr = [...rowCells];
       let textContent = arr.map(item => item.innerHTML);
       
-      document.getElementById("fname").value = textContent[1];
-      document.getElementById("lname").value = textContent[2];
-      document.getElementById("about").value = textContent[3];
-      document.getElementById("color").value = textContent[4];
+      document.getElementById("fname").value = textContent[0];
+      document.getElementById("lname").value = textContent[1];
+      document.getElementById("about").value = textContent[2];
+      document.getElementById("color").value = textContent[3];
       showModal();
     })
     
   });
 
 }
-
-
- 
 
 function returnData () {
   return fetch('json/db.json')
@@ -111,10 +107,28 @@ const hideModal = () => {
 closeBtn.addEventListener('click', hideModal);
 
 function editRow(){
-  table.rows[rowId].cells[1].innerHTML = document.getElementById("fname").value;
-  table.rows[rowId].cells[2].innerHTML = document.getElementById("lname").value;
-  table.rows[rowId].cells[3].innerHTML = document.getElementById("about").value;
-  table.rows[rowId].cells[4].innerHTML = document.getElementById("color").value;
+  table.rows[rowId].cells[0].innerHTML = document.getElementById("fname").value;
+  table.rows[rowId].cells[1].innerHTML = document.getElementById("lname").value;
+  table.rows[rowId].cells[2].innerHTML = document.getElementById("about").value;
+  table.rows[rowId].cells[3].innerHTML = document.getElementById("color").value;
 }
 modalBtn.addEventListener('click', editRow);
+
+const toggleColumnBtn = document.querySelectorAll('.toggle');
+
+function hideColumn() {
+  
+  toggleColumnBtn.forEach(item => {
+    item.addEventListener('click', (e) => {
+      const parentTh = e.target.parentElement;
+      const parentThId = parentTh.getAttribute('id');
+      const selector = '#' + parentThId;
+      const cells = document.querySelectorAll(selector);
+      parentTh.classList.add('hide');
+      console.log(parentTh);
+      cells.forEach(item => item.classList.toggle('hide'));
+    })
+  })
+}
+hideColumn();
 
